95974db36d2b7e705caf01b716ed6ee7bfd0a3ad
Kenny Scelfo <kscelfo@lasersonline.org>
2019-09-26 15:03:32 -0500

    updates for filename change.

--- RoboForm/tools/chocolateyInstall.ps1
+++ RoboForm/tools/chocolateyInstall.ps1
@@ -1,7 +1,40 @@
-﻿$packageName = 'RoboForm'
-$installerType = 'EXE'
-$url = 'https://www.roboform.com/dist/RoboForm-v8-Setup.exe'
-$silentArgs = '/silent'
-$validExitCodes = @(0)
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.roboform.com/dist/RoboForm-v8-Setup.exe'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'roboform*'
+
+  checksum      = 'B69896D7AFCC028EB38A09AB30579E5F9E06C121246A3BB4CC7C18A50E7BF3DD'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/silent"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
