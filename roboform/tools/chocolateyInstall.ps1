8e3046d2a12ad0b650109bebe85a3279cf53c4a2
Kenny Scelfo <kscelfo@gmail.com>
2021-10-04 10:44:01 -0500

    Commit of initial package

---
+++ roboform/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'roboform'
+  fileType               = 'msi'
+  url                    = 'https://www.roboform.com/dist/RoboForm-v9-Enterprise.msi'  
+  checksum               = 'dea45ffb2623d8afefbc1e209aa75d54d47322006e459f2fdcfa5e6e2f71dec8'
+  checksumType           = 'sha256'  
+  silentArgs             = '/qn'
+  validExitCodes         = @(0)
+  softwareName           = 'RoboForm'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ roboform/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://www.roboform.com/download'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url32   = $download_page.links | ? href -match '.msi$' | % href | select -First 1
+    $download_page.AllElements|? innerText -match '.*MSI.*\sv(\d*?\.\d*?\.\d*?)$'
+    $version = $Matches[1]
+
+    @{
+        URL32   = $url32        
+        Version = $version
+    }
+}
+
+update -ChecksumFor 32
